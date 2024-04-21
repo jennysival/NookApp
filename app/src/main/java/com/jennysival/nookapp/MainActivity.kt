5d1f4c5c9bcc13134a1f59014aeed7752a01b2b6
jennysival <jeniffersival@gmail.com>
2024-04-20 23:01:17 -0300

    add app icon, colors, theme and splash screen

---
+++ app/src/main/java/com/jennysival/nookapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jennysival.nookapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.jennysival.nookapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}

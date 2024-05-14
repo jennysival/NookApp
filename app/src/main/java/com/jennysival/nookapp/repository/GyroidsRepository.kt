6f8c63880714474198cdb1bd1f82a8eaa462b319
jennysival <jeniffersival@gmail.com>
2024-05-14 18:33:49 -0300

    add brewster dialogues

--- app/src/main/java/com/jennysival/nookapp/repository/GyroidsRepository.kt
+++ app/src/main/java/com/jennysival/nookapp/repository/GyroidsRepository.kt
@@ -4,6 +4,7 @@ import com.jennysival.nookapp.data.local.gyroids.GyroidsDao
 import com.jennysival.nookapp.data.local.gyroids.GyroidsEntity
 import com.jennysival.nookapp.data.remote.RetrofitService
 import com.jennysival.nookapp.data.remote.gyroids.ApiGyroidsResponseItem
+import com.jennysival.nookapp.ui.gyroids.BrewsterDialogues
 
 class GyroidsRepository(
     private val gyroidsDao: GyroidsDao
@@ -21,5 +22,15 @@ class GyroidsRepository(
 
     suspend fun updateGyroid(gotGyroid: GyroidsEntity) {
         gyroidsDao.updateGyroid(gotGyroid)
+    }
+
+    fun getRandomDialogue(): String {
+        return listOf(
+            BrewsterDialogues.RANDOM_DIALOGUE_ONE,
+            BrewsterDialogues.RANDOM_DIALOGUE_TWO,
+            BrewsterDialogues.RANDOM_DIALOGUE_THREE,
+            BrewsterDialogues.RANDOM_DIALOGUE_FOUR,
+            BrewsterDialogues.RANDOM_DIALOGUE_FIVE
+        ).random()
     }
 }

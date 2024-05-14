11597486d203f5deeffc1bd29e92b62d90f6f838
jennysival <jeniffersival@gmail.com>
2024-05-14 18:33:58 -0300

    add brewster dialogues

---
+++ app/src/main/java/com/jennysival/nookapp/ui/gyroids/BrewsterDialogues.kt
@@ -0,0 +1,19 @@
+package com.jennysival.nookapp.ui.gyroids
+
+class BrewsterDialogues {
+    companion object {
+        const val INITIAL = "Quando um gyroid está comigo, você sabe que está seguro..."
+        const val GIVE_BACK = "Se você quiser seu gyroid de volta, é só me avisar, coo..."
+        const val GOOD_CONDITIONS = "Em ótimas condições, coo..."
+        const val RANDOM_DIALOGUE_ONE =
+            "Eu não me importaria em ver os outros tipos de gyroids que existem, então você quer guardar os seus aqui com os meus?"
+        const val RANDOM_DIALOGUE_TWO =
+            "...Ei. Sem querer me intrometer, mas tô vendo que você tem um gyroid aí..."
+        const val RANDOM_DIALOGUE_THREE =
+            "Você gosta dos gyroids? ...Eu não falo isso pra muitas pessoas, mas eu mesmo sou um colecionador"
+        const val RANDOM_DIALOGUE_FOUR =
+            "Eles não são umas coisinhas incríveis? Não tem nada de muito especial à primeira vista, mas aí quando você escuta seus sons pela primeira vez..."
+        const val RANDOM_DIALOGUE_FIVE =
+            "Colecionar gyroids é uma tarefa árdua, né? Você tem alguma técnica?"
+    }
+}

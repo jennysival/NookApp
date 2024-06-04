250ff0723d4d153bd017057ad0c891bae4f2ec15
jennysival <jeniffersival@gmail.com>
2024-06-04 17:09:21 -0300

    fix loop when api list is empty

--- app/src/main/java/com/jennysival/nookapp/utils/mapper/ApiToDbMapperGyroid.kt
+++ app/src/main/java/com/jennysival/nookapp/utils/mapper/ApiToDbMapperGyroid.kt
@@ -1,9 +1,11 @@
-package com.jennysival.nookapp.data.local.gyroids
+package com.jennysival.nookapp.utils.mapper
 
+import com.jennysival.nookapp.data.local.gyroids.DbGyroid
+import com.jennysival.nookapp.data.local.gyroids.DbVariation
 import com.jennysival.nookapp.data.remote.gyroids.ApiGyroidsResponseItem
 import com.jennysival.nookapp.data.remote.gyroids.Variation
 
-class ApiToDbMapper {
+class ApiToDbMapperGyroid {
 
     fun mapApiToDbGyroid(apiGyroid: ApiGyroidsResponseItem): DbGyroid =
         DbGyroid(name = apiGyroid.name, variationTotal = apiGyroid.variationTotal)
